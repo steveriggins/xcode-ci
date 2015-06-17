c5cd94d71389feb3dc352e06a1d866ad66ac721f
steveriggins <steve@steveriggins.net>
2015-06-16 22:00:23 -0700

    Change back to branch-A (earlier revert failed) and reset test

--- xcode-ciTests/xcode_ciTests.m
+++ xcode-ciTests/xcode_ciTests.m
@@ -41,7 +41,7 @@
 - (void)testSubmoduleA {
     SubmoduleA *submodule = [[SubmoduleA alloc] init];
     NSUInteger result = [submodule submoduleA];
-    NSUInteger expectedResult = 12000;
+    NSUInteger expectedResult = 9000;
     
     XCTAssertEqual(result, expectedResult);
 }
